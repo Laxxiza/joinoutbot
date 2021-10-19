00cb9d61b5f3a0664a9686e4a0496815ce204370
Laxxiza <62009001+Laxxiza@users.noreply.github.com>
2021-10-19 12:10:32 +0300

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -10,8 +10,6 @@ const { setAdmin, getStat } = require('./commands/admins'); //temp
 const { onLine, offLine, timeOut, dinner, fakename } = require('./commands/users');
 const { newChannelHundler, leftChannelHundler, newMember, leftMember, CommandHundler } = require('./hundlers');
 
-//const Markup = require('telegraf/markup');
-
 const bot = new Telegraf(data.token);
 
 const stage = new Stage([ 
@@ -27,27 +25,12 @@ mongoose.connect(
         if (err) {
             return console.log(err);
         }
-        //bot.context.db = client.db("chatAdmin");
         bot.startPolling();
     }
 );
 
 bot.use(session());
 bot.use(stage.middleware());
-bot.telegram.sendMessage("-1001486496396", "Всем спасибо, кто учавствовал в тестировании данного бота. \nНа данный момент работа над ботом приостановлена. \n С любовью, ваш @lunachrysy");
-
-// bot.use((ctx, next) => {
-//     if(ctx.from.username != "lunachrysy") next();
-//     const isPrivate = ctx.chat.type == "private";
-//     if(isPrivate){
-//         console.log("Private");
-//     }
-//     else {
-//         console.log("Not private");
-//     }
-//     next();
-// });
-
 bot.on("text", initSession, isAdmin);
 
 bot.use(privateCommand);
@@ -56,7 +39,6 @@ bot.on("text", CommandHundler, isAdmin);
 
 bot.on("text", Stat, User); //!нужно создание чата или нет
 
-//bot.use(stage.middleware());
 bot.use((ctx, next) => {
     console.log("Context", ctx);
     next();
@@ -71,8 +53,6 @@ bot.hears(/#налинии/, userCommand('online'), onLine());
 bot.hears(/#перерыв/, userCommand('timeout'), timeOut());
 bot.hears(/#обед/, userCommand('dinner'), dinner());
 bot.hears(/#завершил/, userCommand('offline'), offLine());
-bot.hears(/#псевданим/, fakename());
-bot.hears(/#test/, async (ctx) => { ctx.reply(`${ctx.chat.type} ${ctx.chat.id}`) });
 
 bot.on("new_chat_member", newChannelHundler());
 bot.on("left_chat_member", leftChannelHundler());
